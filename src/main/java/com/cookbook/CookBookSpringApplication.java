a8b239bd48c594796691a61036c1873860f0eabe
Philip <schupp@protonmail.com>
2020-05-27 17:09:59 -0400

    spring project cookbook

---
+++ src/main/java/com/cookbook/CookBookSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookBookSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookBookSpringApplication.class, args);
+	}
+
+}
