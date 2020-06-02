ff843a159d9b29adf8c3ce2f85f0f46ae3fc04e4
Jordan Morgan <jordanreedmorgan@gmail.com>
2020-06-02 10:50:24 -0400

    added S3 service

---
+++ src/main/java/com/cookbook/controller/RecipeController.java
@@ -0,0 +1,12 @@
+package com.cookbook.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/recipe")
+public class RecipeController {
+
+}
